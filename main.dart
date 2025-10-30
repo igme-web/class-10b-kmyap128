8234e5129f4b3da06c689ae04998a9c6460ef9c0
Kristin Yap <144386730+kmyap128@users.noreply.github.com>
2025-10-30 12:04:25 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,13 +1,56 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 // 1) You need to install this so it works 'flutter pub add http'
 import 'package:http/http.dart' as http;
 
+import 'package:provider/provider.dart';
+
 //This is where we will fetch some sample JSON (have a look at it please)
 final String postURL = "https://jsonplaceholder.typicode.com/posts";
 
 // 2) ADD your JItem class below (we'll do in class or grab from 10b notes)
+class JItem {
+  final int id;
+  final String title;
 
+  JItem({required this.id, required this.title});
+
+}
+
+class JItemsProvider extends ChangeNotifier {
+  List<JItem> items = [];
+
+  final String postURL = "https://jsonplaceholder.typicode.com/posts";
+
+  Future<void> getData() async {
+
+    var response = await http.get(Uri.parse(postURL));
+
+    if(response.statusCode == 200) {
+      var data = json.decode(response.body);
+
+      for(var item in data) {
+        items.add(
+          JItem(
+            id: item['id'],
+            title: item['title'],
+          )
+        );
+      }
+    }
+    notifyListeners();
+  }
+
+    void clear() {
+    items.clear();
+    notifyListeners();
+  }
+}
+
+
+ 
 void main() {
   runApp(const MainApp());
 }
@@ -17,7 +60,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: DemoPage());
+    return ChangeNotifierProvider(
+      create:(context) => JItemsProvider(),
+      child: const MaterialApp(home: DemoPage())
+    );
   }
 }
 
@@ -31,7 +77,8 @@ class DemoPage extends StatefulWidget {
 class _DemoPageState extends State<DemoPage> {
 
   //3 Add better type checking here use the <JList> we created
-  List data = [];
+  //List data = [];
+  List<JItem> data = [];
 
   @override
   Widget build(BuildContext context) {
@@ -46,12 +93,14 @@ class _DemoPageState extends State<DemoPage> {
                 ElevatedButton(
                   onPressed: () async {
                     // Get data logic here
+                    context.read<JItemsProvider>().getData();
                   },
                   child: Text('Get Data'),
                 ),
                 ElevatedButton(
                   onPressed: () {
                     // Clear data logic here
+                    context.read<JItemsProvider>().clear();
                   },
                   child: Text('Clear Data'),
                 ),
@@ -59,11 +108,11 @@ class _DemoPageState extends State<DemoPage> {
             ),
             Expanded(
               child: ListView.builder(
-                itemCount: data.length,
+                itemCount: context.watch<JItemsProvider>().items.length,
                 itemBuilder: (context, index) {
                   return ListTile(
-                    title: Text(data[index].id.toString()),
-                    subtitle: Text(data[index].title),
+                    title: Text(context.watch<JItemsProvider>().items[index].id.toString()),
+                    subtitle: Text(context.watch<JItemsProvider>().items[index].title),
                   );
                 },
               ),
